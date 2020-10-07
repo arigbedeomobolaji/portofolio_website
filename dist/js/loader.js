d84c691c4009fb2dfa625703df1d0fe5a7e0b25a
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2020-10-08 00:06:30 +0100

    Initil Commit

---
+++ dist/js/loader.js
@@ -0,0 +1,9 @@
+//jshint esversion:6
+
+var loaderContainer = document.querySelector(".loader-container");
+
+window.addEventListener("load", () => {
+  loaderContainer.style.display = "none";
+});
+
+console.log(loaderContainer);
